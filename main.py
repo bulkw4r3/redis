06cd2b4311a79eb2599c4102d9ad53eab9d8d8f3
Eddy Schuhmacher <bulkware@posteo.org>
2021-08-25 17:09:49 +0200

    r.sadd instead of r.set

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ today = datetime.datetime.now()
 stoday = today.isoformat() #convert date to string
 
 r = redis.Redis(host=HOST, port=PORT, password=PASSWORD, db=1, decode_responses=True)
-r.set('mykey', f'Hello from me and you! {stoday}', ex=3600) # ex=expire in Sekunden
-value = r.get('mykey') #ohne decode wird b' davorgesetzt
-print(value, type(value))
+#r.set('mykey', f'Hello from me and you! Timestamp: {stoday}', ex=3600) # ex=expire in Sekunden
+r.sadd('testkey', '7', '6', '8', '9', '11')
+#value = r.get('mykey') #ohne decode wird b' davorgesetzt
+value = r.smembers('testkey')
+print(value)
